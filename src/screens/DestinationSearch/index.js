97e613b430b9f28b7f5eb2d1d773d5d732d3888a
MARCOS <marcossuarezr88@gmail.com>
2023-10-22 02:15:44 +0200

    Suggestion row finish gooD

--- src/screens/DestinationSearch/index.js
+++ src/screens/DestinationSearch/index.js
@@ -1,7 +1,6 @@
 import React, {useState} from "react";
 import { View, TextInput, Text, FlatList, Pressable } from "react-native";
 import styles from './styles.js';
-import Entypo from "react-native-vector-icons/Entypo";
 
 import searchResults from '../../../assets/data/search';
 import {useNavigation} from '@react-navigation/native';
@@ -13,27 +12,26 @@ const DestinationSearchScreen = (props) => {
 
   const navigation = useNavigation();
 
-  const [ inputText, setInputText] = useState('');
-
   return (
     <View style={styles.container}>
-      <TextInput
-        style={styles.textInput}
-        placeholder="Where are you going?"
-        value={inputText}
-        onChangeText={setInputText}
-      />
-
-      <FlatList
-        data={searchResults}
-        renderItem={({item}) => (
-          <Pressable onPress={() => navigation.navigate('Guests') } style={styles.row}>
-            <View style={styles.iconContainer}>
-              <Entypo name={"location-pin"} size={30} />
-            </View>
-            <Text style={styles.locationText}>{item.description}</Text>
-          </Pressable>
-        )}
+      <GooglePlacesAutocomplete
+        placeholder='Searches for Google Places'
+        onPress={(data, details = null) => {
+          // 'details' is provided when fetchDetails = true
+          console.log(data, details);
+          navigation.navigate('Guests');
+        }}
+        fetchDetails
+        styles={{
+          textInput: styles.textInput,
+        }}
+        query={{
+          key: 'AIzaSyBDt1PST1NgtoYLiZdpp51K8A3UQKW5YJE',
+          language: 'en',
+          types: '(cities)',
+        }}
+        suppressDefaultStyles
+        renderRow={(item) => <SuggestionRow item={item} />}
       />
     </View>
   );
